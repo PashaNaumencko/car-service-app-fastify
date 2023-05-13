d3beb172d874d1c8eff7021f6105efaddf46d208
Pavlo Naumenko <pashanaumencko@gmail.com>
2023-05-13 12:50:45 +0300

    feat: add auth and user services, add common exceptions and enums

--- shared/src/common/enums/enums.js
+++ shared/src/common/enums/enums.js
@@ -1,1 +1,3 @@
 export { ApiPath, AuthApiPath } from './api/api.js';
+export { ExceptionMessage, ExceptionName } from './exception/exception.js';
+export { HttpCode, HttpMethod } from './http/http.js';
